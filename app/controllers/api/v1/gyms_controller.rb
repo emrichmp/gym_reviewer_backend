8773b2cdfe1c835d068c4fac2ebdd23e92b21f67
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-08 19:19:47 -0400

    added create action in gymscontroller

--- app/controllers/api/v1/gyms_controller.rb
+++ app/controllers/api/v1/gyms_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::GymsController < ApplicationController
         gyms = Gym.all
         render json: GymSerializer.new(gyms)
     end
+
+    def create
+        @gym = Gym.create(gym_params)
+        if @gym.save
+            render json: @gym, status: :accepted
+        else
+            redner json: { errors: @gym.errors.fullmessages }, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def gym_params
+        params.require(:gym).permit(:name, :description, :location)
+    end
 end
