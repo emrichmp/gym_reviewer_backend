ea48f5e94e9572512ceac43ea5de01071e2e0889
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-08 14:40:57 -0400

    added reviews controller actions, index and create

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::ReviewsController < ApplicationController
-    
+
+    def index
+        reviews = Review.all
+        render json: reviews
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            render json: @review, status: :accepted
+        else
+            redner json: { errors: @review.errors.fullmessages }, status: :unprocessible_entity
+        end
+    end
+
+
+    private
+
+    def review_params
+        params.require(:review).permit(:content, :rating, :gym_id)
+    end
 end
