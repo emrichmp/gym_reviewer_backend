f84a37fe78f10183cbde4102d13c5470d816673a
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-04-12 13:25:42 -0400

    added create route to gyms

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
         resources :reviews, only: [:index]
-        resources :gyms, only: [:index]
+        resources :gyms, only: [:index, :create]
     end
   end
 
